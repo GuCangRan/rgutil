7b1b5039da1a3e27f4eae9191f5c73dd27eac91d
GUCANGRAN\GuaiWenWo <1067454662@qq.com>
2021-01-14 15:49:51 +0800

    fix bug

--- utils/window.js
+++ utils/window.js
@@ -12,12 +12,17 @@ export const goToTop = (isAnimation = false) => {
         return true;
     }
     //滚动到顶部
+    scrollToTop();
+    return true;
+}
+
+//滚动到顶部
+const scrollToTop = () => {
     const c = document.documentElement.scrollTop || document.body.scrollTop;
     if (c > 0) {
         window.requestAnimationFrame(scrollToTop);
         window.scrollTo(0, c - c / 8);
     }
-    return true;
 }
 
 /**
